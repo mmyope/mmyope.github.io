5b6dd0b7b1f1746e458ab895abe5802bbcae3842
Ariana Freitag <Ariana@Abels-MacBook-Pro.local>
2025-02-19 21:30:37 -0500

    upload temp homepage

--- js/toggle-demo.js
+++
@@ -1,9 +0,0 @@
-function toggle_visibility(elementId) {
-  console.log(elementId)
-  var x = document.getElementById(elementId);
-  if (x.style.display == 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
-}
